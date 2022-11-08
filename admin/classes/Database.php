03db15bc031ab6a6f004c5b395255a77ed017c5a
Fabrice <57037980+Fabricefabfab@users.noreply.github.com>
2022-11-08 17:25:24 +0200

    Update Database.php

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "shoppn");
+		$this->con = new Mysqli("us-cdbr-east-06.cleardb.net", "bc2fdbe3d9fb92", "3f1ab0a9", "heroku_0d961f286c98afb");
 		return $this->con;
 	}
 }
